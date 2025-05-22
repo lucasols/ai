26f844dd628244cbed5c2b4150ed0528a64c6572
lucaslos <lucas@sugestly.com>
2025-05-22 08:27:25 -0300

    Update tsup configuration to include additional entry points and specify output directory

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/main.ts'],
+  entry: ['src/main.ts', 'src/fileProcessing.ts', 'src/schemaBuilder.ts'],
+  outDir: 'dist',
   clean: true,
   format: ['esm', 'cjs'],
 });
